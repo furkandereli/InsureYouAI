13aeabe1b716a97e71705658f78455ff8db6e1ff
furkandereli <furkandereli98@gmail.com>
2025-10-02 17:42:41 +0300

    Add project files.

---
+++ InsureYouAI/Entities/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace InsureYouAI.Entities;
+
+public class Category
+{
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; }
+    public List<Category> Categories { get; set; }
+}
