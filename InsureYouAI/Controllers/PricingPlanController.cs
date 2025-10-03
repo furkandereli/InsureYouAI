f2c5a99c984f7d62a2ca767a7f823c8dd60b3f29
furkandereli <furkandereli98@gmail.com>
2025-10-03 22:58:45 +0300

    PricingPlan operations

---
+++ InsureYouAI/Controllers/PricingPlanController.cs
@@ -0,0 +1,52 @@
+﻿using InsureYouAI.Context;
+using InsureYouAI.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAI.Controllers
+{
+    public class PricingPlanController(InsureContext context) : Controller
+    {
+        public IActionResult PricingPlanList()
+        {
+            var values = context.PricingPlans.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreatePricingPlan()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreatePricingPlan(PricingPlan pricingPlan)
+        {
+            context.PricingPlans.Add(pricingPlan);
+            context.SaveChanges();
+            return RedirectToAction("PricingPlanList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdatePricingPlan(int id)
+        {
+            var value = context.PricingPlans.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdatePricingPlan(PricingPlan pricingPlan)
+        {
+            var value = context.PricingPlans.Update(pricingPlan);
+            context.SaveChanges();
+            return RedirectToAction("PricingPlanList");
+        }
+
+        public IActionResult DeletePricingPlan(int id)
+        {
+            var value = context.PricingPlans.Find(id);
+            context.PricingPlans.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("PricingPlanList");
+        }
+    }
+}
