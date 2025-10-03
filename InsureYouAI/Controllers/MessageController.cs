ec5b57902fc2f6b808a4a015cf2398f462097732
furkandereli <furkandereli98@gmail.com>
2025-10-03 22:36:44 +0300

    Message operations

---
+++ InsureYouAI/Controllers/MessageController.cs
@@ -0,0 +1,54 @@
+﻿using InsureYouAI.Context;
+using InsureYouAI.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAI.Controllers
+{
+    public class MessageController(InsureContext context) : Controller
+    {
+        public IActionResult MessageList()
+        {
+            var values = context.Messages.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateMessage(Message message)
+        {
+            message.IsRead = false;
+            message.SendDate = DateTime.Now;
+            context.Messages.Add(message);
+            context.SaveChanges();
+            return RedirectToAction("MessageList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateMessage(int id)
+        {
+            var value = context.Messages.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateMessage(Message message)
+        {
+            var value = context.Messages.Update(message);
+            context.SaveChanges();
+            return RedirectToAction("MessageList");
+        }
+
+        public IActionResult DeleteMessage(int id)
+        {
+            var value = context.Messages.Find(id);
+            context.Messages.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("MessageList");
+        }
+    }
+}
