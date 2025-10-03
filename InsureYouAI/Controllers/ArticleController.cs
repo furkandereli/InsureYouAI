f52db7967041d16d6240aaf58be66a9eaf9e77d5
furkandereli <furkandereli98@gmail.com>
2025-10-04 00:23:21 +0300

    Article operations

---
+++ InsureYouAI/Controllers/ArticleController.cs
@@ -0,0 +1,52 @@
+﻿using InsureYouAI.Context;
+using InsureYouAI.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAI.Controllers;
+
+public class ArticleController(InsureContext context) : Controller
+{
+    public IActionResult ArticleList()
+    {
+        var values = context.Articles.ToList();
+        return View(values);
+    }
+
+    [HttpGet]
+    public IActionResult CreateArticle()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult CreateArticle(Article article)
+    {
+        article.CreatedDate = DateTime.Now;
+        context.Articles.Add(article);
+        context.SaveChanges();
+        return RedirectToAction("ArticleList");
+    }
+
+    [HttpGet]
+    public IActionResult UpdateArticle(int id)
+    {
+        var value = context.Articles.Find(id);
+        return View(value);
+    }
+
+    [HttpPost]
+    public IActionResult UpdateArticle(Article article)
+    {
+        var value = context.Articles.Update(article);
+        context.SaveChanges();
+        return RedirectToAction("ArticleList");
+    }
+
+    public IActionResult DeleteArticle(int id)
+    {
+        var value = context.Articles.Find(id);
+        context.Articles.Remove(value);
+        context.SaveChanges();
+        return RedirectToAction("ArticleList");
+    }
+}
