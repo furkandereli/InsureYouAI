d7e6405e8b80dca5db499c33ea28ab24d7188dc2
furkandereli <furkandereli98@gmail.com>
2025-10-06 01:24:08 +0300

    Identity and sign up

---
+++ InsureYouAI/Controllers/RegisterController.cs
@@ -0,0 +1,34 @@
+﻿using InsureYouAI.Context;
+using InsureYouAI.DTOs.Register;
+using InsureYouAI.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAI.Controllers
+{
+    public class RegisterController(UserManager<AppUser> userManager) : Controller
+    {
+        [HttpGet]
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(CreateUserRegisterDTO createUser)
+        {
+            AppUser appUser = new()
+            {
+                Name = createUser.Name,
+                Surname = createUser.Surname,
+                Email = createUser.Email,
+                UserName = createUser.Username,
+                ImageUrl = "Test",
+                Description = "Açıklama"
+            };
+
+            await userManager.CreateAsync(appUser, createUser.Password);
+            return RedirectToAction("UserList");
+        }
+    }
+}
---
+++ InsureYouAI/DTOs/Register/CreateUserRegisterDTO.cs
@@ -0,0 +1,3 @@
+﻿namespace InsureYouAI.DTOs.Register;
+
+public record CreateUserRegisterDTO(string Name, string Surname, string Username, string Email, string Password);
