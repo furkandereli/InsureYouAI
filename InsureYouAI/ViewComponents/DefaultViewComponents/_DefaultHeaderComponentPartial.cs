6111a43cb24ae960df06a109593e4239af56d060
furkandereli <furkandereli98@gmail.com>
2025-10-08 17:25:16 +0300

    Component partials

---
+++ InsureYouAI/ViewComponents/DefaultViewComponents/_DefaultHeaderComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAI.ViewComponents.DefaultViewComponents;
+
+public class _DefaultHeaderComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
---
+++ InsureYouAI/ViewComponents/DefaultViewComponents/_DefaultMobileMenuComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using InsureYouAI.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAI.ViewComponents.DefaultViewComponents;
+
+public class _DefaultMobileMenuComponentPartial(InsureContext context) : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        ViewBag.email = context.Contacts.Select(x => x.Email).FirstOrDefault();
+        ViewBag.phone = context.Contacts.Select(x => x.Phone).FirstOrDefault();
+        return View();
+    }
+}
