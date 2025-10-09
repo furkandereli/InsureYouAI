ff5ce267713d800e1bf8e126d232432dd6cb60c5
furkandereli <furkandereli98@gmail.com>
2025-10-09 20:31:04 +0300

    About and about items

---
+++ InsureYouAI/ViewComponents/DefaultViewComponents/_DefaultAboutComponentPartial.cs
@@ -0,0 +1,18 @@
+﻿using InsureYouAI.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAI.ViewComponents.DefaultViewComponents;
+
+public class _DefaultAboutComponentPartial(InsureContext context) : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        ViewBag.title = context.Abouts.Select(a => a.Title).FirstOrDefault();
+        ViewBag.description = context.Abouts.Select(a => a.Description).FirstOrDefault();
+        ViewBag.imageUrl = context.Abouts.Select(a => a.ImageUrl).FirstOrDefault();
+
+        var aboutItemValues = context.AboutItems.ToList();
+
+        return View(aboutItemValues);
+    }
+}
